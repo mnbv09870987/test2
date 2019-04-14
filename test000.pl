fb58e469da44a128bdcba99046c4ddebf4a0fcb4
Eugen Lut <misslsv@mail.ru>
2019-04-14 13:42:01 +0300

    Телефон 01

---
+++ test000.pl
@@ -0,0 +1,15 @@
+use strict;
+use warnings;
+
+
+my @x =  ( 13, 15, 3 );
+my $a = 5;
+my $b = 3;
+
+
+for my $i ( @x ) {print "$i  \n";}
+print " \n";
+
+print  " Square ",  $a + $b;
+print   "\n";
+
